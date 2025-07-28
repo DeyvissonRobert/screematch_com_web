43a061b3b84125e993a5c013d6bcf5d37857ec63
DeyvissonRobert <robert.designerx@gmail.com>
2025-07-28 18:57:10 -0300

    Modelando dados da série

--- src/main/java/br/com/alura/screematch_spring/ScreematchSpringApplication.java
+++ src/main/java/br/com/alura/screematch_spring/ScreematchSpringApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screematch_spring;
 
+import br.com.alura.screematch_spring.model.DadosSerie;
 import br.com.alura.screematch_spring.services.ConsumoApi;
+import br.com.alura.screematch_spring.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +17,13 @@ public class ScreematchSpringApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=The+Originals&Season=1&apikey=92dab9e4");
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=The+Originals&apikey=92dab9e4");
 		System.out.println(json);
 //		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json"); //Imagem de Café
 //		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 
 	}
 }
