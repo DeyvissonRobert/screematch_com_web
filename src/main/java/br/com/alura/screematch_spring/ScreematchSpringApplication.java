bb1c4f73ee38202435c3b002801d414a8237035d
DeyvissonRobert <robert.designerx@gmail.com>
2025-07-28 17:41:10 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/screematch_spring/ScreematchSpringApplication.java
@@ -0,0 +1,18 @@
+package br.com.alura.screematch_spring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreematchSpringApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreematchSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Primeiro projeto Spring sem Web!");
+	}
+}
---
+++ src/test/java/br/com/alura/screematch_spring/ScreematchSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screematch_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreematchSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
