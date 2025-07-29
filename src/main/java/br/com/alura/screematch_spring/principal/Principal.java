3f1c061c08a418b5c8c7d7fd92f8d5105879592d
DeyvissonRobert <robert.designerx@gmail.com>
2025-07-29 19:28:32 -0300

    Buscando episódios a partir de uma data

--- src/main/java/br/com/alura/screematch_spring/principal/Principal.java
+++ src/main/java/br/com/alura/screematch_spring/principal/Principal.java
@@ -7,6 +7,8 @@ import br.com.alura.screematch_spring.model.Episodio;
 import br.com.alura.screematch_spring.services.ConsumoApi;
 import br.com.alura.screematch_spring.services.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -66,6 +68,21 @@ public class Principal {
 
         episodios.forEach(System.out::println);
 
+        System.out.println("A partir de que ano você deseja ver os episódios? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                ", Episódio: " + e.getTitulo() +
+                                ", Data lançamento: " + e.getDataLancamento().format(formatador)
+                ));
     }
 
 }
