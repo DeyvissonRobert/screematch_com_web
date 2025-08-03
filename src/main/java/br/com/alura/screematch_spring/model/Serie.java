d83d9cdbe9839041ef032839a23b91b4883f56e8
DeyvissonRobert <robert.designerx@gmail.com>
2025-08-03 11:49:22 -0300

    Mapeando as entidades
    # Conflicts:
    #	src/main/java/br/com/alura/screematch_spring/model/Serie.java
    #	src/main/resources/application.properties

--- src/main/java/br/com/alura/screematch_spring/model/Serie.java
+++ src/main/java/br/com/alura/screematch_spring/model/Serie.java
@@ -1,6 +1,7 @@
 package br.com.alura.screematch_spring.model;
 
-import br.com.alura.screematch_spring.service.ConsultaChatGPT;
+import br.com.alura.screematch_spring.service.tradução.ConsultaChatGPT;
+import br.com.alura.screematch_spring.service.tradução.ConsultaMyMemory;
 
 import java.util.OptionalDouble;
 
@@ -20,7 +21,7 @@ public class Serie {
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
+        this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse()).trim();
     }
 
     public String getSinopse() {
