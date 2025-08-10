f427fefe28f4e8e9f5cdc158cb2a3cda02889be8
DeyvissonRobert <robert.designerx@gmail.com>
2025-08-10 18:44:17 -0300

    Filtrando episódios por data de lançamento - Fim de + um curso.

--- src/main/java/br/com/alura/screematch_spring/principal/Principal.java
+++ src/main/java/br/com/alura/screematch_spring/principal/Principal.java
@@ -42,6 +42,7 @@ public class Principal {
                     8 - Filtrar séries
                     9 - Buscar episódios por trecho
                     10 - Top 5 episódios por série
+                    11 - Buscar episódios a partir de uma data
                     
                     0 - Sair
                     =====================================    
@@ -82,6 +83,9 @@ public class Principal {
                     break;
                 case 10:
                     topEpisodiosPorSerie();
+                    break;
+                case 11:
+                    buscarEpisodiosDepoisDeUmaData();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -218,6 +222,19 @@ public class Principal {
                     System.out.printf("Série: %s Temporada %s - Episódio %s - %s Avaliação %s\n",
                             e.getSerie().getTitulo(), e.getTemporada(),
                             e.getNumeroEpisodio(), e.getTitulo(), e.getAvaliacao() ));
+        }
+    }
+
+    private void buscarEpisodiosDepoisDeUmaData(){
+        buscarSeriePorTitulo();
+        if(serieBusca.isPresent()){
+            Serie serie = serieBusca.get();
+            System.out.println("Digite o ano limite de lançamento");
+            var anoLancamento = leitura.nextInt();
+            leitura.nextLine();
+
+            List<Episodio> episodiosAno = repositorio.episodiosPorSerieEAno(serie, anoLancamento);
+            episodiosAno.forEach(System.out::println);
         }
     }
 
