2f51822456b7e56c1cccdaf29c6cc15cba39a318
DeyvissonRobert <robert.designerx@gmail.com>
2025-08-14 17:56:43 -0300

    Utilizando o Spring Web

---
+++ src/main/java/br/com/alura/screematch_spring/ScreensoundApplicationSemWeb.java
@@ -0,0 +1,17 @@
+package br.com.alura.screematch_spring;
+
+//@SpringBootApplication
+//public class ScreensoundApplicationSemWeb implements CommandLineRunner {
+//	@Autowired
+//	private ArtistaRepository repositorio;
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ScreensoundApplicationSemWeb.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Principal principal = new Principal(repositorio);
+//		principal.exibeMenu();
+//	}
+//}
