4d864ce8f201b6ec4c95aa51048643bc7542f361
DeyvissonRobert <robert.designerx@gmail.com>
2025-08-14 19:04:39 -0300

    Devolvendo séries no navegador

--- src/main/java/br/com/alura/screematch_spring/controller/SerieController.java
+++ src/main/java/br/com/alura/screematch_spring/controller/SerieController.java
@@ -1,13 +1,20 @@
 package br.com.alura.screematch_spring.controller;
 
+import br.com.alura.screematch_spring.model.Serie;
+import br.com.alura.screematch_spring.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SerieController {
+    @Autowired
+    private SerieRepository repositorio;
 
     @GetMapping("/series")
-    public String obterSeries() {
-        return "Aqui vão ser listadas as séries";
+    public List<Serie> obterSeries() {
+        return repositorio.findAll();
     }
 }
