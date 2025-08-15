26eca014bad5289ec56dc5839753d3da4a948601
DeyvissonRobert <robert.designerx@gmail.com>
2025-08-15 18:44:55 -0300

    Exibindo as top séries do banco

--- src/main/java/br/com/alura/screematch_spring/service/SerieService.java
+++ src/main/java/br/com/alura/screematch_spring/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.alura.screematch_spring.service;
 
 import br.com.alura.screematch_spring.dto.SerieDTO;
+import br.com.alura.screematch_spring.model.Serie;
 import br.com.alura.screematch_spring.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,16 @@ public class SerieService {
     private SerieRepository repositorio;
 
     public List<SerieDTO> obterTodasAsSeries() {
-        return repositorio.findAll()
-                .stream()
+        return converteDados(repositorio.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series) {
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
     }
